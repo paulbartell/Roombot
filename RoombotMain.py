810af5056ed07980a60c2a063ace9fb8e4847ea3
Paul Bartell <paul.bartell@gmail.com>
2012-12-11 16:23:32 -0800

    Keyboard-based control module working

--- RoombotMain.py
+++ RoombotMain.py
@@ -2,25 +2,86 @@
 
 from RoombaSCI import RoombaAPI
 from stormLauncher import launchControl
-import pyserial
+import serial
 from EmgInterface import EmgInterface
+import time
+from TestUI import TestUI
+from copy import deepcopy
 
 ROOMBA_PORT="/dev/tty.usbserial-A2001n69"
 ROOMBA_BAUD="115200"
 
-HAPTICS_PORT="/dev/rfcomm0"
-HAPTICS_BAUD="115200"
+#HAPTICS_PORT="/dev/rfcomm0"
+#HAPTICS_BAUD="115200"
 
-EMG_PORT="/dev/rfcomm1"
-EMG_BAUD="115200"
+#EMG_PORT="/dev/rfcomm1"
+#EMG_BAUD="115200"
+
+launcher = launchControl()
+roomba = RoombaAPI(ROOMBA_PORT, ROOMBA_BAUD)
+
+#emg = EmgInterface(EMG_PORT, EMG_BAUD);
+#emg = EmgInterface("/dev/tty.M13762-BluetoothSerialP",115200)
+ui = TestUI()
+
+#State variable [forward,left,right,vacuum on,vacuum off,m-up,m-down,m-fire]
 
 
-
-launcher = launchControl();
-roomba = RoombaAPI(ROOMBA_PORT, ROOMBA_BAUD);
-emg = EmgInterface(EMG_PORT, EMG_BAUD);
+roomba.connect()
+roomba.safe() #set safe control mode
 
 
+def processState(state,oldstate):
+	if state[0:3] == [0,0,0]: #roomba stop condition
+		roomba.stop()
+		print "stop"
+	if state[0] == 1 and oldstate[0] == 0: #Go forward
+		roomba.forward()
+		print "forward"
+	if state[1] == 1 and oldstate[1] == 0: #Go left
+		roomba.left()
+	if state[2] == 1 and oldstate[2] == 0: #Go Right
+		roomba.right()
+	if state[3] == 1 and oldstate[3] == 0: #Vacuum on
+		roomba.motors(0b00000111)
+	if state[3] == 0 and oldstate[3] == 1: #Vacuum on -stop
+		pass
+	if state[4] == 1 and oldstate[4] == 0: #Vacuum off
+		roomba.motors(0b00000000)
+	if state[4] == 0 and oldstate[4] == 1: #vacuum off - stop
+		pass
+	if state[5] == 1 and oldstate[5] == 0: # turret up
+		launcher.turretUp()
+		print "up"
+	if state[5] == 0 and oldstate[5] == 1: # turret up stop
+		launcher.turretStop()
+	if state[6] == 1 and oldstate[6] == 0: # turret down
+		launcher.turretDown()
+		print "down"
+	if state[6] == 0 and oldstate[6] == 1: # turret down stop
+		launcher.turretStop()
+	if state[7] == 1 and oldstate[7] == 0: #turret fire
+		launcher.turretFire()
+	if state[7] == 0 and oldstate[7] == 1: #turret fire stop
+		pass
+	print "got to the end"
+	return
+# Main loop
+
+oldstate = [0,0,0,0,0,0,0,0]
+state = [0,0,0,0,0,0,0,0]
 
 
-emg = EmgInterface("/dev/tty.M13762-BluetoothSerialP",115200)
+while True:
+	ui.getState()
+	state = ui.state
+	print oldstate
+	print ui.state
+	print "\n"
+	if not (state == oldstate):
+		processState(state,oldstate)
+		print "state transition performed"
+	oldstate = deepcopy(ui.state)
+	time.sleep(0.01) #loop rate 100Hz
+	
+
---
+++ TestUI.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import pygame, sys,os
+from pygame.locals import * 
+
+class TestUI():
+
+	def __init__(self):
+		pygame.init()
+		window = pygame.display.set_mode((60, 60)) 
+		pygame.display.set_caption('Roombot controller') 
+		screen = pygame.display.get_surface() 
+		pygame.display.flip()
+		self.state = [0,0,0,0,0,0,0,0]
+		self.keyList = [K_UP, K_LEFT, K_RIGHT, K_PERIOD, K_SLASH, K_RETURN, K_RSHIFT, K_SPACE]
+
+	def getState(self):
+	
+		for event in pygame.event.get(): 
+			if event.type == QUIT: 
+				print event
+				sys.exit(0)
+			elif event.type == KEYDOWN:
+				try:
+					bit = self.keyList.index(event.key)
+					self.state[bit] = 1
+					print "set bit on: " + str(bit)
+				except ValueError:
+					#Nothing happens here cause we dont care about that key
+					pass
+			elif event.type == KEYUP:
+				try:
+					bit = self.keyList.index(event.key)
+					self.state[bit] = 0
+					print "set bit off: " + str(bit)
+				except ValueError:
+					pass
+					#Nothing happens here cause we dont care about that key
+		return
