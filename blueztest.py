dfe3d62437d6024833676461d45121368d657bcb
Paul Bartell <paul.bartell@gmail.com>
2013-02-11 17:56:07 -0800

    --

--- blueztest.py
+++ blueztest.py
@@ -3,32 +3,39 @@
 
 
 import bluetooth
+import time
+import binascii
 
 deviceId = '00:07:80:4B:F4:2B'
 
 print "performing inquiry..."
 
-nearby_devices = bluetooth.discover_devices(lookup_names = True)
+#nearby_devices = bluetooth.discover_devices(lookup_names = True)
 
-print "found %d devices" % len(nearby_devices)
+#print "found %d devices" % len(nearby_devices)
 
-for addr, name in nearby_devices:
-    if addr == deviceId:
-		print addr
+#for addr, name in nearby_devices:
+#    if addr == deviceId:
+#		print addr
 
 port = 1
 
 sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 sock.connect((deviceId, port))
-
-
+#sock.settimeout(1)
 #Send N command
-sock.send(chr(int('11101110',2)))
-sock.recv(18)
 
-# Sending B command would be ideal
-sock.send(chr(int('01000010',2)))
-sock.recv(24)
-# receive 24 bytes.. loop every 10mS or so... or wait till start
+for j in range(0,100):
+	vals = []
+	for i in range(0,8):
+		sock.send(chr(0x80 + ord(str(i))))
+
+	for k in range(0,8):
+		for l in range(0,5)
+			packet[l] = sock.recv(1)
+		msb = ord(packet[1]) << 8
+		lsb = ord(packet[2])
+		vals.append(lsb + msb)
+	print vals
 
 
